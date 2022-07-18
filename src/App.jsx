405a5c1952b5ae19446e3dd134131674008fca4d
unknown <ts.createlink@gmail.com>
2022-07-18 23:12:38 +0900

    add description about analytics

--- src/App.jsx
+++ src/App.jsx
@@ -119,6 +119,11 @@ function App() {
             }
           </ul>
         </section>
+        <small>
+          <p>当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。</p>
+          <p>このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。</p>
+          <p> この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関しての詳細は<a href="https://marketingplatform.google.com/about/analytics/terms/jp/" rel='noopener'> Googleアナリティクスサービス利用規約</a>のページや<a href="https://policies.google.com/technologies/ads?hl=ja" rel="noopener">Googleポリシーと規約</a>ページをご覧ください。</p>
+        </small>
       </div>
     </div>
   )
